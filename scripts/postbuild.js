3392a7036809a7693ddbe9ca786d093ae2fc086b
Karol Fabjańczuk <karol-f@users.noreply.github.com>
2024-07-05 18:33:28 +0200

    Fix node imports (#48)
    * fix: import from Node.js
    
    * fix: import from Node.js

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -10,8 +10,8 @@ const packageJsonData = await fs.readFile(path.resolve(packagePath, './package.j
 const packageJson = JSON.parse(packageJsonData);
 
 // Modify the package.json object
-packageJson.main = 'index.js';
-packageJson.module = 'cjs/index.js';
+packageJson.main = 'cjs/index.js';
+packageJson.module = 'index.js';
 packageJson.types = 'index.d.ts';
 packageJson.exports = {
   '.': {
@@ -31,9 +31,9 @@ packageJson.exports = {
 // Remove devDependencies
 delete packageJson.devDependencies;
 delete packageJson.scripts;
-delete packageJson.type;
 
 // Write the modified package.json to the build folder
 fs.writeJsonSync(path.resolve(buildPath, './package.json'), packageJson, { spaces: 2 });
+fs.writeJsonSync(path.resolve(path.join(buildPath, 'cjs'), './package.json'), { "type": "commonjs" }, { spaces: 2 });
 
 console.log('package.json has been modified and copied to the build folder.');
