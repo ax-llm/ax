985e0f080d5cfae1148cc9d707e5f82bfbc4b18a
Poly <106871581+polydeuxes@users.noreply.github.com>
2024-07-19 16:11:11 -0400

    Fixes

--- src/ax/ai/ollama/info.ts
+++ src/ax/ai/ollama/info.ts
@@ -9,39 +9,34 @@ export const axModelInfoOllama: AxModelInfo[] = [
     currency: 'N/A',
     characterIsToken: false,
     promptTokenCostPer1M: 0,
-    completionTokenCostPer1M: 0,
-    computeCost: 1
+    completionTokenCostPer1M: 0
   },
   {
     name: AxAIOllamaModel.Llama2,
     currency: 'N/A',
     characterIsToken: false,
     promptTokenCostPer1M: 0,
-    completionTokenCostPer1M: 0,
-    computeCost: 1.5
+    completionTokenCostPer1M: 0
   },
   {
     name: 'all-MiniLM',
     currency: 'N/A',
     characterIsToken: false,
     promptTokenCostPer1M: 0,
-    completionTokenCostPer1M: 0,
-    computeCost: 0.5
+    completionTokenCostPer1M: 0
   },
   {
     name: 'Llama2-7B',
     currency: 'N/A',
     characterIsToken: false,
     promptTokenCostPer1M: 0,
-    completionTokenCostPer1M: 0,
-    computeCost: 2
+    completionTokenCostPer1M: 0
   },
   {
     name: 'Llama2-13B',
     currency: 'N/A',
     characterIsToken: false,
     promptTokenCostPer1M: 0,
-    completionTokenCostPer1M: 0,
-    computeCost: 2.5
+    completionTokenCostPer1M: 0
   }
 ];
