7dab2dfcbead565df7be878e4822ff472b878b35
Poly <106871581+polydeuxes@users.noreply.github.com>
2024-07-20 09:22:30 -0400

    Convert Ollama API files from JS to TS

---
+++ src/ax/ai/ollama/info.ts
@@ -0,0 +1,41 @@
+import type { AxModelInfo } from '../types.js';
+
+import { AxAIOllamaModel } from './types.js';
+
+export const axModelInfoOllama: AxModelInfo[] = [
+  {
+    name: AxAIOllamaModel.Codellama,
+    currency: 'compute',
+    characterIsToken: false,
+    promptTokenCostPer1M: 1,
+    completionTokenCostPer1M: 1
+  },
+  {
+    name: AxAIOllamaModel.Llama2,
+    currency: 'compute',
+    characterIsToken: false,
+    promptTokenCostPer1M: 1,
+    completionTokenCostPer1M: 1
+  },
+  {
+    name: AxAIOllamaModel.MiniLM,
+    currency: 'compute',
+    characterIsToken: false,
+    promptTokenCostPer1M: 0.5,
+    completionTokenCostPer1M: 0.5
+  },
+  {
+    name: AxAIOllamaModel.Llama2_7B,
+    currency: 'compute',
+    characterIsToken: false,
+    promptTokenCostPer1M: 0.7,
+    completionTokenCostPer1M: 0.7
+  },
+  {
+    name: AxAIOllamaModel.Llama2_13B,
+    currency: 'compute',
+    characterIsToken: false,
+    promptTokenCostPer1M: 1.3,
+    completionTokenCostPer1M: 1.3
+  }
+];
