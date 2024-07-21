bc461d50d4ba8a6efda5a00507003a856fe8b1f4
dosco <832235+dosco@users.noreply.github.com>
2024-07-21 00:11:50 -0700

    Revert "Fix: Convert Ollama API files from JS to TS (#52)"
    This reverts commit e77dce496af79fdaf2f6d0ace6eb3e5ac161275b.

--- src/ax/ai/ollama/api.ts
+++ src/ax/ai/ollama/api.ts
@@ -1,61 +1,46 @@
-import type { API } from '../../util/apicall.js';
 import {
-  AxBaseAI,
   axBaseAIDefaultConfig,
   axBaseAIDefaultCreativeConfig
 } from '../base.js';
-import type {
-  AxAIServiceOptions,
-  AxChatResponse,
-  AxEmbedResponse,
-  AxInternalChatRequest,
-  AxInternalEmbedRequest,
-  AxModelConfig
-} from '../types.js';
+import { AxAIOpenAI } from '../openai/api.js';
+import type { AxAIOpenAIConfig } from '../openai/types.js';
+import type { AxAIServiceOptions } from '../types.js';
 
-import { axModelInfoOllama } from './info.js';
-import type { AxAIOllamaChatError } from './types.js';
-import type {
-  AxAIOllamaChatRequest,
-  AxAIOllamaChatResponse,
-  AxAIOllamaChatResponseDelta,
-  AxAIOllamaConfig,
-  AxAIOllamaEmbedRequest,
-  AxAIOllamaEmbedResponse
-} from './types.js';
-import { AxAIOllamaEmbedModel, AxAIOllamaModel } from './types.js';
+export type AxAIOllamaAIConfig = AxAIOpenAIConfig;
 
-export const axAIOllamaDefaultConfig = (): AxAIOllamaConfig => ({
-  model: AxAIOllamaModel.Codellama,
-  embedModel: AxAIOllamaEmbedModel.Codellama,
-  ...axBaseAIDefaultConfig()
-});
+export const axAIOllamaDefaultConfig = (): AxAIOllamaAIConfig =>
+  structuredClone({
+    ...axBaseAIDefaultConfig(),
+    model: 'nous-hermes2',
+    embedModel: 'all-minilm'
+  });
 
-export const axAIOllamaDefaultCreativeConfig = (): AxAIOllamaConfig => ({
-  model: AxAIOllamaModel.Codellama,
-  embedModel: AxAIOllamaEmbedModel.Codellama,
-  ...axBaseAIDefaultCreativeConfig()
-});
+export const axAIOllamaDefaultCreativeConfig = (): AxAIOllamaAIConfig =>
+  structuredClone({
+    ...axBaseAIDefaultCreativeConfig(),
+    model: 'nous-hermes2',
+    embedModel: 'all-minilm'
+  });
 
-export interface AxAIOllamaArgs {
+export type AxAIOllamaArgs = {
   name: 'ollama';
+  model?: string;
+  embedModel?: string;
   url?: string;
-  config?: Readonly<Partial<AxAIOllamaConfig>>;
+  apiKey?: string;
+  config?: Readonly<Partial<AxAIOllamaAIConfig>>;
   options?: Readonly<AxAIServiceOptions>;
-  modelMap?: Record<string, AxAIOllamaModel | AxAIOllamaEmbedModel | string>;
-}
+  modelMap?: Record<string, string>;
+};
 
-export class AxAIOllama extends AxBaseAI<
-  AxAIOllamaChatRequest,
-  AxAIOllamaEmbedRequest,
-  AxAIOllamaChatResponse,
-  AxAIOllamaChatResponseDelta,
-  AxAIOllamaEmbedResponse
-> {
-  private config: AxAIOllamaConfig;
-
+/**
+ * OllamaAI: AI Service
+ * @export
+ */
+export class AxAIOllama extends AxAIOpenAI {
   constructor({
-    url,
+    apiKey = 'not-set',
+    url = 'http://localhost:11434',
     config,
     options,
     modelMap
@@ -65,234 +50,13 @@ export class AxAIOllama extends AxBaseAI<
       ...config
     };
     super({
-      name: 'Ollama',
-      apiURL: new URL('/api', url || 'http://localhost:11434').href,
-      headers: {},
-      modelInfo: axModelInfoOllama,
-      models: {
-        model: _config.model,
-        embedModel: _config.embedModel
-      },
+      apiKey,
       options,
-      supportFor: { functions: false, streaming: true },
+      config: _config,
+      apiURL: new URL('/api', url).href,
       modelMap
     });
 
-    this.config = _config;
+    super.setName('Ollama');
   }
-
-  override getModelConfig(): AxModelConfig {
-    const { config } = this;
-    return {
-      maxTokens: config.maxTokens,
-      temperature: config.temperature,
-      topP: config.topP,
-      topK: config.topK,
-      stream: config.stream ?? false
-    };
-  }
-
-  override generateChatReq = (
-    req: Readonly<AxInternalChatRequest>
-  ): [API, AxAIOllamaChatRequest] => {
-    const model = req.model;
-
-    const apiConfig: API = {
-      name: '/chat'
-    };
-
-    const messages = req.chatPrompt.map((msg) => {
-      if (msg.role === 'function') {
-        return {
-          role: msg.role,
-          content: msg.result,
-          name: msg.functionId
-        };
-      }
-      return {
-        role: msg.role,
-        content:
-          typeof msg.content === 'string'
-            ? msg.content
-            : JSON.stringify(msg.content)
-      };
-    });
-
-    const reqValue: AxAIOllamaChatRequest = {
-      model,
-      messages,
-      stream: req.modelConfig?.stream ?? this.config.stream ?? false,
-      options: {
-        temperature: req.modelConfig?.temperature ?? this.config.temperature,
-        top_p: req.modelConfig?.topP ?? this.config.topP,
-        top_k: req.modelConfig?.topK ?? this.config.topK,
-        num_predict: req.modelConfig?.maxTokens ?? this.config.maxTokens
-      }
-    };
-
-    return [apiConfig, reqValue];
-  };
-
-  override generateChatResp = (
-    resp: Readonly<AxAIOllamaChatResponse>
-  ): AxChatResponse => {
-    return {
-      results: [
-        {
-          content: resp.message?.content || '',
-          finishReason: resp.done_reason || 'stop'
-        }
-      ],
-      modelUsage: resp.total_duration
-        ? {
-            totalTokens: (resp.prompt_eval_count ?? 0) + (resp.eval_count ?? 0),
-            promptTokens: resp.prompt_eval_count ?? 0,
-            completionTokens: resp.eval_count ?? 0
-          }
-        : undefined
-    };
-  };
-
-  override generateChatStreamResp = (
-    resp: Readonly<AxAIOllamaChatResponseDelta>,
-    state: object
-  ): AxChatResponse => {
-    const sstate = state as {
-      fullContent: string;
-    };
-
-    if (!sstate.fullContent) {
-      sstate.fullContent = '';
-    }
-
-    switch (resp.type) {
-      case 'message_start':
-        // Handle message start
-        return {
-          results: [{ content: '' }]
-        };
-
-      case 'content_block_start':
-        // Handle content block start
-        sstate.fullContent += resp.content_block.text;
-        return {
-          results: [
-            {
-              content: resp.content_block.text
-            }
-          ]
-        };
-
-      case 'content_block_delta':
-        // Handle content block delta
-        sstate.fullContent += resp.delta.text;
-        return {
-          results: [
-            {
-              content: resp.delta.text
-            }
-          ]
-        };
-
-      case 'message_delta':
-        // Handle message delta
-        if ('done' in resp.delta && resp.delta.done) {
-          return {
-            results: [
-              {
-                content: sstate.fullContent,
-                finishReason: resp.delta.done_reason || 'stop'
-              }
-            ],
-            modelUsage: resp.delta.total_duration
-              ? {
-                  totalTokens:
-                    (resp.delta.prompt_eval_count ?? 0) +
-                    (resp.delta.eval_count ?? 0),
-                  promptTokens: resp.delta.prompt_eval_count ?? 0,
-                  completionTokens: resp.delta.eval_count ?? 0
-                }
-              : undefined
-          };
-        }
-        break;
-
-      case 'message_stop':
-        // Handle message stop
-        return {
-          results: [
-            {
-              content: sstate.fullContent,
-              finishReason: 'stop'
-            }
-          ]
-        };
-
-      case 'content_block_stop':
-        // Handle content block stop
-        break;
-
-      case 'ping':
-        // Handle ping
-        break;
-
-      case 'error':
-        // Handle error
-        return {
-          results: [
-            {
-              content: `Error: ${(resp as AxAIOllamaChatError).error.message}`,
-              finishReason: 'error'
-            }
-          ],
-          modelUsage: undefined
-        };
-    }
-
-    return {
-      results: [
-        {
-          content: sstate.fullContent
-        }
-      ]
-    };
-  };
-
-  override generateEmbedReq = (
-    req: Readonly<AxInternalEmbedRequest>
-  ): [API, AxAIOllamaEmbedRequest] => {
-    const embedModel = req.embedModel;
-
-    if (!embedModel) {
-      throw new Error('Embed model not set');
-    }
-
-    if (!req.texts || req.texts.length === 0) {
-      throw new Error('Embed texts is empty');
-    }
-
-    const apiConfig: API = {
-      name: '/embeddings'
-    };
-
-    const reqBody: AxAIOllamaEmbedRequest = {
-      model: embedModel,
-      prompt: req.texts
-    };
-
-    return [apiConfig, reqBody];
-  };
-
-  override generateEmbedResp = (
-    resp: Readonly<AxAIOllamaEmbedResponse>
-  ): AxEmbedResponse => {
-    return {
-      embeddings: [resp.embedding],
-      modelUsage: {
-        totalTokens: resp.token_count,
-        promptTokens: resp.token_count,
-        completionTokens: 0
-      }
-    };
-  };
 }
