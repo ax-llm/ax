fee21e23a04c779bcd10d43581d12df2510d6bcc
dosco <832235+dosco@users.noreply.github.com>
2025-01-14 01:22:32 -0800

    fix: build issue

--- src/docs/astro.config.mjs
+++ src/docs/astro.config.mjs
@@ -1,8 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools,, tailwind()],
+  integrations: [tailwind()],
 });
--- src/docs/src/env.d.ts
+++ src/docs/src/env.d.ts
@@ -3,6 +3,3 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module 'astro-imagetools/components';
-declare module 'astro-imagetools/api';
