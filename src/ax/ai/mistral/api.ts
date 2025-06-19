2f3d4d6208c1067df0e26824ef5bc582ace9dde1
Marc Buils <marc.buils@marcbuils.fr>
2025-06-19 05:01:39 +0200

    fix(mistral): remove unsupported params and fix image compatibility (#247)
    * fix: mistral max_completion_token not exist and set topP to 1
    
    * fix: update mistral messages for image_url compatibility
    
    ---------
    
    Co-authored-by: Marc BUILS <marc.buils@presta.tessi.fr>

--- src/ax/ai/mistral/api.ts
+++ src/ax/ai/mistral/api.ts
@@ -1,6 +1,9 @@
 import { axBaseAIDefaultConfig } from '../base.js'
 import { type AxAIOpenAIArgs, AxAIOpenAIBase } from '../openai/api.js'
-import type { AxAIOpenAIConfig } from '../openai/chat_types.js'
+import type {
+  AxAIOpenAIChatRequest,
+  AxAIOpenAIConfig,
+} from '../openai/chat_types.js'
 import type { AxAIServiceOptions, AxModelInfo } from '../types.js'
 
 import { axModelInfoMistral } from './info.js'
@@ -15,6 +18,7 @@ export const axAIMistralDefaultConfig = (): AxAIMistralConfig =>
   structuredClone({
     model: AxAIMistralModel.MistralSmall,
     ...axBaseAIDefaultConfig(),
+    topP: 1,
   })
 
 export const axAIMistralBestConfig = (): AxAIMistralConfig =>
@@ -22,6 +26,46 @@ export const axAIMistralBestConfig = (): AxAIMistralConfig =>
     ...axAIMistralDefaultConfig(),
     model: AxAIMistralModel.MistralLarge,
   })
+
+export type AxAIMistralChatRequest = Omit<
+  AxAIOpenAIChatRequest<AxAIMistralModel>,
+  'max_completion_tokens' | 'stream_options' | 'messages'
+> & {
+  max_tokens?: number
+  messages: (
+    | { role: 'system'; content: string }
+    | {
+        role: 'user'
+        content:
+          | string
+          | (
+              | {
+                  type: 'text'
+                  text: string
+                }
+              | {
+                  type: 'image_url'
+                  image_url: string
+                }
+            )[]
+        name?: string
+      }
+    | {
+        role: 'assistant'
+        content: string
+        name?: string
+        tool_calls?: {
+          type: 'function'
+          function: {
+            name: string
+            // eslint-disable-next-line functional/functional-parameters
+            arguments?: string
+          }
+        }[]
+      }
+    | { role: 'tool'; content: string; tool_call_id: string }
+  )[]
+}
 
 export type AxAIMistralArgs = AxAIOpenAIArgs<
   'mistral',
@@ -60,6 +104,24 @@ export class AxAIMistral extends AxAIOpenAIBase<
       hasShowThoughts: false,
     }
 
+    // Chat request updater to add Grok's search parameters
+    const chatReqUpdater = (
+      req: Readonly<AxAIOpenAIChatRequest<AxAIMistralModel>>
+    ): AxAIMistralChatRequest => {
+      // eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
+      const { max_completion_tokens, stream_options, messages, ...result } =
+        req as AxAIOpenAIChatRequest<AxAIMistralModel> & {
+          stream_options?: unknown
+        }
+
+      return {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ...(result as any),
+        messages: this.updateMessages(messages),
+        max_tokens: max_completion_tokens,
+      }
+    }
+
     super({
       apiKey,
       config: _config,
@@ -68,8 +130,40 @@ export class AxAIMistral extends AxAIOpenAIBase<
       modelInfo,
       models,
       supportFor,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      chatReqUpdater: chatReqUpdater as any,
     })
 
     super.setName('Mistral')
+  }
+
+  private updateMessages(
+    messages: AxAIOpenAIChatRequest<AxAIMistralModel>['messages']
+  ) {
+    const messagesUpdated = []
+
+    if (!Array.isArray(messages)) {
+      return messages
+    }
+
+    for (const message of messages) {
+      if (message.role === 'user' && Array.isArray(message.content)) {
+        const contentUpdated = message.content.map((item) => {
+          if (
+            typeof item === 'object' &&
+            item !== null &&
+            item.type === 'image_url'
+          ) {
+            return { type: 'image_url', image_url: item.image_url?.url }
+          }
+          return item
+        })
+        messagesUpdated.push({ ...message, content: contentUpdated })
+      } else {
+        messagesUpdated.push(message)
+      }
+    }
+
+    return messagesUpdated
   }
 }
