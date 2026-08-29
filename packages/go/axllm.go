30f706438059f95ad86c575be393b6594647045c
Spacy <832235+dosco@users.noreply.github.com>
2026-08-29 09:31:05 -0700

    chore: release v24.0.14 (#630)

--- packages/go/axllm.go
+++ packages/go/axllm.go
@@ -65149,4 +65149,4 @@ func extractQuotedSuffix(s string) (Value, error) {
 
 func _core_type_is_json(value Value) Value { return true }
 
-func Version() string { return "24.0.13" }
+func Version() string { return "24.0.14" }
